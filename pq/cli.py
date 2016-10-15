1e5fe7d88bca5ad046d24cfa37091016b4877382
Bernardas <bernardas.alisauskas@gmail.com>
2016-10-15 18:37:14 +0000

    adjust help text wording

--- pq/cli.py
+++ pq/cli.py
@@ -7,8 +7,8 @@ from pq import PQ
 @click.command()
 @click.argument('query')
 @click.option('--css', help='use css selectors instead of xpath', is_flag=True)
-@click.option('-t', '--text', 'to_text', help='automatically add text', is_flag=True)
-@click.option('-tt', '--text_all', 'to_text_all', help='automatically add text', is_flag=True)
+@click.option('-t', '--text', 'to_text', help='get only the text (no markup)', is_flag=True)
+@click.option('-tt', '--text_all', 'to_text_all', help='like to_text but gets all text including node\'s children', is_flag=True)
 @click.option('-f', '--first', 'first', help='only the first element', is_flag=True)
 # @click.option('-p', '--pretty', 'to_pretty', help='prettify the output', is_flag=True)
 def cli(query, css, to_text, to_text_all, first):
