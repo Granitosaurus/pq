a061aebe1779acc1f8a4f70d25af738323a6acf2
Bernardas <bernardas.alisauskas@gmail.com>
2016-10-16 21:23:15 +0000

    add support for file as an argument

--- pq/cli.py
+++ pq/cli.py
@@ -1,18 +1,24 @@
-import click
 import sys
+
+import click
 
 from pq import PQ
 
 
 @click.command()
 @click.argument('query')
+@click.argument('infile', type=click.File('r'), required=False)
 @click.option('--css', help='use css selectors instead of xpath', is_flag=True)
 @click.option('-t', '--text', 'to_text', help='get only the text (no markup)', is_flag=True)
 @click.option('-tt', '--text_all', 'to_text_all', help='like to_text but gets all text including node\'s children', is_flag=True)
 @click.option('-f', '--first', 'first', help='only the first element', is_flag=True)
-# @click.option('-p', '--pretty', 'to_pretty', help='prettify the output', is_flag=True)
-def cli(query, css, to_text, to_text_all, first):
-    piped_data = ''.join(sys.stdin)
+def cli(query, infile, css, to_text, to_text_all, first):
+    """Command line xml and json processor for xpath and css selectors.
+    """
+    if infile:
+        piped_data = infile.read()
+    else:
+        piped_data = ''.join(sys.stdin)
     pq = PQ(piped_data)
     if css:
         result = pq.css(query, to_text=to_text, to_text_all=to_text_all)
