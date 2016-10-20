856e3999becb7bc8133eab495e5090bd781ae919
Bernardas <bernardas.alisauskas@gmail.com>
2016-10-20 08:04:04 +0000

    add compact flag

--- pq/cli.py
+++ pq/cli.py
@@ -10,23 +10,23 @@ from pq import PQ
 @click.argument('infile', type=click.File('r'), required=False)
 @click.option('--css', help='use css selectors instead of xpath', is_flag=True)
 @click.option('-t', '--text', 'to_text', help='get only the text (no markup)', is_flag=True)
-@click.option('-tt', '--text_all', 'to_text_all', help='like to_text but gets all text including node\'s children', is_flag=True)
+@click.option('-c', '--compact', 'compact', help='compact instead of pretty-printed output', is_flag=True)
+@click.option('-tt', '--text_all', 'to_text_all',
+              help='like to_text but gets all text including node\'s children', is_flag=True)
 @click.option('-f', '--first', 'first', help='only the first element', is_flag=True)
-def cli(query, infile, css, to_text, to_text_all, first):
+def cli(query, infile, css, to_text, to_text_all, first, compact):
     """Command line xml and json processor for xpath and css selectors.
     """
     if infile:
         piped_data = infile.read()
     else:
         piped_data = ''.join(sys.stdin)
-    pq = PQ(piped_data)
-    if css:
-        result = pq.css(query, to_text=to_text, to_text_all=to_text_all)
-    else:
-        result = pq.xpath(query, to_text=to_text, to_text_all=to_text_all)
+    pq = PQ(piped_data, to_text=to_text, to_text_all=to_text_all)
+    result = pq.css(query) if css else pq.xpath(query)
     if first:
         result = result[0] if result else None
-    pq.output(result)
+    if result:
+        pq.output(result, compact)
 
 if __name__ == "__main__":
     cli()
