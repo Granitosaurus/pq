1d33f69f509edc6e1d208bd31b45c584007541f8
Bernardas <bernardas.alisauskas@gmail.com>
2016-10-21 10:52:00 +0000

    rework output

--- pq/cli.py
+++ pq/cli.py
@@ -10,11 +10,12 @@ from pq import PQ
 @click.argument('infile', type=click.File('r'), required=False)
 @click.option('--css', help='use css selectors instead of xpath', is_flag=True)
 @click.option('-t', '--text', 'to_text', help='get only the text (no markup)', is_flag=True)
+@click.option('-j', '--json', 'to_json', help='[experimental] convert xml output to json', is_flag=True)
 @click.option('-c', '--compact', 'compact', help='compact instead of pretty-printed output', is_flag=True)
-@click.option('-tt', '--text_all', 'to_text_all',
+@click.option('-tt', '--text-all', 'to_text_all',
               help='like to_text but gets all text including node\'s children', is_flag=True)
 @click.option('-f', '--first', 'first', help='only the first element', is_flag=True)
-def cli(query, infile, css, to_text, to_text_all, first, compact):
+def cli(query, infile, css, to_text, to_text_all, first, compact, to_json):
     """Command line xml and json processor for xpath and css selectors.
     """
     if infile:
@@ -26,7 +27,7 @@ def cli(query, infile, css, to_text, to_text_all, first, compact):
     if first:
         result = result[0] if result else None
     if result:
-        pq.output(result, compact)
+        click.echo(pq.output(result, compact=compact, to_json=to_json))
 
 if __name__ == "__main__":
     cli()
--- pq/pq.py
+++ pq/pq.py
@@ -2,9 +2,26 @@ import re
 import json
 from json.decoder import JSONDecodeError
 from collections import OrderedDict
+from lxml import etree
 
 from parsel import Selector
 from dicttoxml import dicttoxml
+
+RE_CLEAN = re.compile('\s{2,}')
+RE_HAS_TEXT = re.compile('text\(\)$|::text$')
+RE_IS_HTML = re.compile('<.+?>')
+
+
+def clean_text(text):
+    """clean text for repetitive spaces"""
+    text = text.strip()
+    text = RE_CLEAN.sub(' ', text)
+    return text
+
+
+def is_xml(text):
+    """check whether text stirng is xml or html code"""
+    return bool(RE_IS_HTML.match(text.strip()))
 
 
 class PQ:
@@ -65,19 +82,6 @@ class PQ:
         """
         return self._path('css', path, to_text, to_text_all, first)
 
-    @staticmethod
-    def process_results(results, first_available=False):
-        """
-        process list of results.
-        :param results: list of nodes or strings
-        :param first_available: only take first value
-        :return: processed results
-        """
-        if first_available:
-            r = [r for r in results]
-            return r[0] if r else None
-        return results
-
     def process_path(self, path, func_name=None, to_text=None, to_text_all=None):
         """
         process xpath or css selector path.
@@ -87,13 +91,9 @@ class PQ:
         :param to_text_all: retrieve node's all text rather than xml code
         :return: processed path
         """
-        if to_text is None:
-            to_text = self.to_text
-        if to_text_all is None:
-            to_text_all = self.to_text_all
-        if re.findall('text\(\)|::text', path):
-            # already has text
-            return path
+        to_text = self.to_text if to_text is None else to_text
+        to_text_all = self.to_text_all if to_text_all is None else to_text_all
+
         css = func_name == 'css'
         addon = ''
         if css:
@@ -102,20 +102,45 @@ class PQ:
         else:
             if to_text or to_text_all:
                 addon = '/text()' if to_text else '//text()'
+
+        # xpath can have multiple paths
         paths = path.split('|') if not css else [path]
-        path = '|'.join(p + addon for p in paths)
+        # check if paths already have xpath
+        paths = [p for p in paths if not RE_HAS_TEXT.findall(path)]
+        path = '|'.join(p + addon for p in paths) if paths else path
         return path
 
-    @staticmethod
-    def output(results, compact=False):
+    def output(self, results, compact=False, to_json=False, to_text=None, to_text_all=None):
         """
         outputs results to stdout
+        :param results: list of result
         :param compact: compact output
-        :param results: list of results
-        :return: None
+        :param to_json: output as json
+        :param to_text: output as text [defaults to self.to_text]
+        :param to_text_all: output as all text (that is under node) [defaults to self.to_text_all]
+        :return: result string
         """
+        def result_to_json(r):
+            # todo rework, only works for nodes with text
+            # for example it will fail for <a> or <img> nodes if they have no text
+            sel = Selector(text=r)
+            name = sel.xpath('name(//*[text()])').extract_first()
+            value = ''.join(sel.xpath('//text()').extract()).strip()
+            return {name: value}
+        # defaults
+        to_text = self.to_text if to_text is None else to_text
+        to_text_all = self.to_text_all if to_text_all is None else to_text_all
+
+        if to_text_all:
+            return clean_text(''.join(results))
+        elif to_text or not is_xml(''.join(results)):
+            return clean_text('\n'.join(results))
+        elif to_json:
+            result = json.dumps([result_to_json(r) for r in results], indent=2 if not compact else 0,)
+            return result
+        results = Selector(text=''.join(results))
         if compact:
-            result = json.dumps(results)
+            return results
         else:
-            result = json.dumps(results, indent=4)
-        print(result.replace('\\"', '"'))
+            return etree.tostring(results.root, pretty_print=True)
+
